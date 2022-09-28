6d6d432de873555471315c60a4e91fdc0de9ea99
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-09-28 10:53:55 -0500

    Start of List model and Context

--- GroceryGrabber/Models/ListContext.cs
+++ GroceryGrabber/Models/ListContext.cs
@@ -4,5 +4,22 @@ namespace GroceryGrabber.Models
 {
     public class ListContext : DbContext
     {
+        public ListContext(DbContextOptions<ListContext> options)
+: base(options)
+        { }
+
+        public DbSet<Lists> Lists { get; set; }
+        public DbSet<GroceryItem> GroceryItems { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //base.OnModelCreating(modelBuilder);
+            //modelBuilder.Entity<Lists>().HasData();
+        }
     }
 }
