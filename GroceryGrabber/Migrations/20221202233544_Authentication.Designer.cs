b9a3f4daf1277627da8ff96e3f0c59b0caf61bcc
Taylor Wilkens <twilkens@dmacc.edu>
2022-12-03 14:49:11 -0600

    Log in creation, Admin creation, and security.

--- GroceryGrabber/Migrations/20221202233544_Authentication.Designer.cs
+++ GroceryGrabber/Migrations/20221202233544_Authentication.Designer.cs
@@ -4,6 +4,7 @@ using GroceryGrabber.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GroceryGrabber.Migrations
 {
     [DbContext(typeof(GroceryContext))]
-    partial class GroceryContextModelSnapshot : ModelSnapshot
+    [Migration("20221202233544_Authentication")]
+    partial class Authentication
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -103,8 +105,11 @@ namespace GroceryGrabber.Migrations
 
             modelBuilder.Entity("GroceryGrabber.Models.UserModel", b =>
                 {
-                    b.Property<string>("Id")
-                        .HasColumnType("nvarchar(450)");
+                    b.Property<int>("UserId")
+                        .ValueGeneratedOnAdd()
+                        .HasColumnType("int");
+
+                    SqlServerPropertyBuilderExtensions.UseIdentityColumn(b.Property<int>("UserId"), 1L, 1);
 
                     b.Property<int>("AccessFailedCount")
                         .HasColumnType("int");
@@ -118,16 +123,28 @@ namespace GroceryGrabber.Migrations
                     b.Property<bool>("EmailConfirmed")
                         .HasColumnType("bit");
 
+                    b.Property<string>("Id")
+                        .HasColumnType("nvarchar(max)");
+
                     b.Property<bool>("LockoutEnabled")
                         .HasColumnType("bit");
 
                     b.Property<DateTimeOffset?>("LockoutEnd")
                         .HasColumnType("datetimeoffset");
 
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(10)
+                        .HasColumnType("nvarchar(10)");
+
                     b.Property<string>("NormalizedEmail")
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("NormalizedUserName")
+                        .HasColumnType("nvarchar(max)");
+
+                    b.Property<string>("Password")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("PasswordHash")
@@ -146,11 +163,60 @@ namespace GroceryGrabber.Migrations
                         .HasColumnType("bit");
 
                     b.Property<string>("UserName")
-                        .HasColumnType("nvarchar(max)");
+                        .IsRequired()
+                        .HasMaxLength(8)
+                        .HasColumnType("nvarchar(8)");
 
-                    b.HasKey("Id");
+                    b.HasKey("UserId");
 
                     b.ToTable("Users");
+
+                    b.HasData(
+                        new
+                        {
+                            UserId = 1,
+                            AccessFailedCount = 0,
+                            ConcurrencyStamp = "b28ce0c2-8a41-42cb-ac2f-0ed703a21629",
+                            EmailConfirmed = false,
+                            Id = "acdd5a56-4c36-4d5c-a0a5-08de958d31ea",
+                            LockoutEnabled = false,
+                            Name = "Morgan",
+                            Password = "P@ssw0rd1",
+                            PhoneNumberConfirmed = false,
+                            SecurityStamp = "0ec521d2-7fbf-4492-8254-80f2cdb555e0",
+                            TwoFactorEnabled = false,
+                            UserName = "MorganC"
+                        },
+                        new
+                        {
+                            UserId = 2,
+                            AccessFailedCount = 0,
+                            ConcurrencyStamp = "e6ccfb11-fe71-4ae6-866c-cea1dcb89a81",
+                            EmailConfirmed = false,
+                            Id = "38ae9764-0653-41f8-9a6f-6c23c9853f05",
+                            LockoutEnabled = false,
+                            Name = "Taylor",
+                            Password = "P@ssw0rd1",
+                            PhoneNumberConfirmed = false,
+                            SecurityStamp = "fd7ebf7c-94ee-4b81-829c-4e99773a8165",
+                            TwoFactorEnabled = false,
+                            UserName = "LadyLuck"
+                        },
+                        new
+                        {
+                            UserId = 3,
+                            AccessFailedCount = 0,
+                            ConcurrencyStamp = "bcb6c40f-661b-4feb-9719-f17670db4344",
+                            EmailConfirmed = false,
+                            Id = "f263091e-b143-4a8f-9acd-d36cc0404ec4",
+                            LockoutEnabled = false,
+                            Name = "Deaon",
+                            Password = "P@ssw0rd1",
+                            PhoneNumberConfirmed = false,
+                            SecurityStamp = "b3aa50fd-9138-416b-b2e7-7287f4e3c2a7",
+                            TwoFactorEnabled = false,
+                            UserName = "DeonD"
+                        });
                 });
 
             modelBuilder.Entity("GroceryGrabber.Models.UsersLists", b =>
@@ -170,16 +236,21 @@ namespace GroceryGrabber.Migrations
                     b.Property<int>("UserID")
                         .HasColumnType("int");
 
-                    b.Property<string>("UserId")
-                        .HasColumnType("nvarchar(450)");
-
                     b.HasKey("id");
 
                     b.HasIndex("ItemGroceryId");
 
-                    b.HasIndex("UserId");
+                    b.HasIndex("UserID");
 
                     b.ToTable("GroceryList");
+
+                    b.HasData(
+                        new
+                        {
+                            id = 1,
+                            GroceryId = 1,
+                            UserID = 1
+                        });
                 });
 
             modelBuilder.Entity("GroceryGrabber.Models.UsersLists", b =>
@@ -190,7 +261,9 @@ namespace GroceryGrabber.Migrations
 
                     b.HasOne("GroceryGrabber.Models.UserModel", "User")
                         .WithMany()
-                        .HasForeignKey("UserId");
+                        .HasForeignKey("UserID")
+                        .OnDelete(DeleteBehavior.Cascade)
+                        .IsRequired();
 
                     b.Navigation("Item");
 
---
+++ GroceryGrabber/Migrations/20221202233544_Authentication.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GroceryGrabber.Migrations
+{
+    public partial class Authentication : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AccessFailedCount",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConcurrencyStamp",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailConfirmed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Id",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "LockoutEnabled",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LockoutEnd",
+                table: "Users",
+                type: "datetimeoffset",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedEmail",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NormalizedUserName",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordHash",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PhoneNumber",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "PhoneNumberConfirmed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SecurityStamp",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "TwoFactorEnabled",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 1,
+                columns: new[] { "ConcurrencyStamp", "Id", "SecurityStamp" },
+                values: new object[] { "b28ce0c2-8a41-42cb-ac2f-0ed703a21629", "acdd5a56-4c36-4d5c-a0a5-08de958d31ea", "0ec521d2-7fbf-4492-8254-80f2cdb555e0" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 2,
+                columns: new[] { "ConcurrencyStamp", "Id", "SecurityStamp" },
+                values: new object[] { "e6ccfb11-fe71-4ae6-866c-cea1dcb89a81", "38ae9764-0653-41f8-9a6f-6c23c9853f05", "fd7ebf7c-94ee-4b81-829c-4e99773a8165" });
+
+            migrationBuilder.UpdateData(
+                table: "Users",
+                keyColumn: "UserId",
+                keyValue: 3,
+                columns: new[] { "ConcurrencyStamp", "Id", "SecurityStamp" },
+                values: new object[] { "bcb6c40f-661b-4feb-9719-f17670db4344", "f263091e-b143-4a8f-9acd-d36cc0404ec4", "b3aa50fd-9138-416b-b2e7-7287f4e3c2a7" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AccessFailedCount",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ConcurrencyStamp",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Id",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnabled",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "LockoutEnd",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedEmail",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "NormalizedUserName",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumber",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PhoneNumberConfirmed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "SecurityStamp",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "TwoFactorEnabled",
+                table: "Users");
+        }
+    }
+}
