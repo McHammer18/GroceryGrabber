e391ba5b421a654aae4420c2873c2d4c18546e56
McHammer18 <65956506+McHammer18@users.noreply.github.com>
2022-12-09 18:14:03 -0600

    Dependency Injection start

---
+++ GroceryGrabber/Repository/GroceryRepository.cs
@@ -0,0 +1,45 @@
+﻿using GroceryGrabber.Migrations;
+using GroceryGrabber.Models;
+
+namespace GroceryGrabber.Repository
+{
+    public class GroceryRepository : IGroceryRepository
+    {
+        private GroceryContext context;
+        public GroceryRepository(GroceryContext ctx)
+        {
+            context = ctx;
+        }
+
+        public List<GroceryItem> GetAllItems()
+        {
+            return context.GroceryItems.ToList();
+        }
+
+        public GroceryItem Find(int id)
+        {
+            return context.GroceryItems.Find(id);
+        }
+
+        public void DeleteGroceryItem(GroceryItem groceryItem)
+        {
+            context.GroceryItems.Remove(groceryItem);
+            context.SaveChanges();
+        }
+
+        public void InsertGroceryItem(GroceryItem groceryItem)
+        {
+            context.Add(groceryItem);
+        }
+
+        public void Save()
+        {
+            context.SaveChanges();
+        }
+
+        public void UpdateGroceryItem(GroceryItem groceryItem)
+        {
+            context.GroceryItems.Update(groceryItem);
+        }
+    }
+}
---
+++ GroceryGrabber/Repository/IGroceryRepository.cs
@@ -0,0 +1,14 @@
+﻿using GroceryGrabber.Models;
+
+namespace GroceryGrabber.Repository
+{
+    public class IGroceryRepository
+    {
+        List<GroceryItem> GetAllItems();
+        GroceryItem Find(int id);
+        void Save();
+        void InsertGroceryItem(GroceryItem groceryItem);
+        void DeleteGroceryItem(GroceryItem groceryItem);
+        void UpdateGroceryItem(GroceryItem groceryItem);
+    }
+}
