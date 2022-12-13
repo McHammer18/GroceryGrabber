4df438c01b35f9335a2db4e47a53603cbbcec120
xlovelyladyluck <38335658+xlovelyladyluck@users.noreply.github.com>
2022-12-13 10:53:51 -0600

    Unit testing

--- GroceryTest/UnitTest1.cs
+++ GroceryTest/UnitTest1.cs
@@ -1,10 +1,13 @@
+using System;
 using System.Net.Sockets;
 using System.Xml.Schema;
 using GroceryGrabber.Controllers;
 using GroceryGrabber.Migrations;
 using GroceryGrabber.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Logging;
 using Moq;
 
@@ -12,36 +15,64 @@ namespace GroceryTest
 {
     public class UnitTest1
     {
+        private GroceryContext ctx;
+
+        
+
+        public UnitTest1() //Makes db, edit things for db
+        {
+            DbContextOptions<GroceryContext>inmemory = new DbContextOptionsBuilder<GroceryContext>().UseInMemoryDatabase("Filename=test.db").ConfigureWarnings(x => x.Ignore(InMemoryEventId.TransactionIgnoredWarning)).Options;
+            ctx = new GroceryContext(inmemory);
+            ctx.GroceryList.Add(new UsersLists()
+            {
+                GroceryID = 1,
+                User = new UserModel(),
+                id = 1,
+                Item = new GroceryItem()
+                {
+                    GroceryId = 1,
+                    Name = "1lbs Hamburger",
+                    Description = "Roll of ground Hamburger Meat",
+                    Department = "Meat",
+                    Location = "Meat Counter/ Aisle 65"
+                }
+            });
+            ctx.SaveChanges();
+        }
+     
         [Fact]
         public void listCreationTest()
         {
             //Arange
-            //var logger = new Mock<ILogger<ListController>>();
-            var ulMock = new Mock<GroceryViewModel>();
-            List<GroceryViewModel> lst = new List<GroceryViewModel>()
-            {
-                new GroceryViewModel() {GroceryID = 1, UserID = "1", GroceryName="Test List", item1="turkey", item2="grapes", item3="oatmeal", item4="tissues"},
-                new GroceryViewModel() {GroceryID = 2, UserID = "2", GroceryName="Test List Two", item1="ham", item2="crackers", item3="cheese whiz", item4="mini diet-coke"}
-            };
-
-            //ulMock.Setup(r => r.Create(new GroceryViewModel()).Returns(lst);
-
-            ListController ctrl = new ListController(GroceryContext, userMgr );
-
+            var store = new Mock<IUserStore<UserModel>>();
+            var mgr = new Mock<UserManager<UserModel>>(store.Object, null, null, null, null, null, null, null, null);
+            
+            ListController ctrl = new ListController(ctx , mgr.Object );
+            
             //action
-            var result = ctrl.Index("all");
+            var result = ctrl.Open();
 
             
-
             //Assert
-            Assert.IsInstanceOfType(result, typeof(ViewResult));
-            var vr = result as ViewResult;
-            Assert.IsInstanceOfType(vr.Model, typeof(List<UsersLists>));
-            var mdl = vr.Model as List<UsersLists>;
-            Assert.AreEqual(2, mdl.Count());
-
+            var vr = Assert.IsType<ViewResult>(result);
+            var mdl = Assert.IsType<List<UsersLists>>(vr.Model);
+            
+            Assert.Equal(1, mdl.Count);
+            var itm = mdl[0];
+            
+            Assert.NotNull(itm.User);
+            Assert.NotNull(itm.Item);
         }
     }
-
+    //public IActionResult Open()
+    //{
+    //    //This is grabbing the person's list
+    //    var itm = context.GroceryList
+    //        .Include(gl => gl.User)
+    //        .Include(gl => gl.Item)
+    //        .ToList();
+    //    return View("~/Views/List/ViewGroceryList.cshtml", itm);
+    //}
+    //
     //home controller test
 }
